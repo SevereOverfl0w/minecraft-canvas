b0750a19f9aabb7e4ccf3fad64e77a6f9903c502
SevereOverfl0w <overfl0w@narna.co>
2014-09-17 10:12:24 +0100

    New interface. All in one function now. Code still needs work.

--- mcCanvas.js
+++ mcCanvas.js
@@ -5,71 +5,119 @@ mcCanvas = {
             posY: 8,
             width: 8,
             height: 8,
-            destX: 4,
-            destY: 0
         },
         hat: {
             posX: 40,
             posY: 8,
             width: 8,
             height: 8,
-            destX: 4,
-            destY: 0
         },
         body: {
             posX: 20,
             posY: 20,
             width: 8,
             height: 12,
-            destX: 4,
-            destY: 8
         },
         leftLeg: {
             posX: 4,
             posY: 20,
             width: 4,
             height: 12,
-            destX: 4,
-            destY: 20
         },
         rightLeg: {
             posX: 4,
             posY: 20,
             width: 4,
             height: 12,
-            destX: 8,
-            destY: 20
         },
         leftArm: {
             posX: 44,
             posY: 20,
             width: 4,
             height: 12,
-            destX: 0,
-            destY: 8
         },
         rightArm: {
             posX: 44,
             posY: 20,
             width: 4,
             height: 12,
-            destX: 12,
-            destY: 8
         }
     },
     
-    renderSkin: function(element, options){
-        scale = options.scale;
+    overrides: {
+        face: {
+            head: {
+                destX: 0,
+                destY: 0
+            },
+            hat: {
+                destX: 0,
+                destY: 0
+            }
+        },
+        body: {
+            head: {
+                destX: 4,
+                destY: 0
+            },
+            hat: {
+                destX: 4,
+                destY: 0
+            },
+            body: {
+                destX: 4,
+                destY: 8
+            },
+            leftLeg: {
+                destX: 4,
+                destY: 20
+            },
+            rightLeg: {
+                destX: 8,
+                destY: 20
+            },
+            leftArm: {
+                destX: 0,
+                destY: 8
+            },
+            rightArm: {
+                destX: 12,
+                destY: 8
+            }
+        }
+    
+    },
+    
+    renderMinecraft: function(style, element, options){
+        var scale = options.scale;
         var canvas = document.createElement('canvas');
         canvas.classList = element.classList;
         canvas.id = element.id;
+        config = this.config;
+        overrides = this.overrides;
     
-        canvas.width = (this.config.leftArm.width + this.config.body.width + this.config.rightArm.width)*scale;
-        canvas.height = (this.config.head.height + this.config.body.height + this.config.leftLeg.height)*scale;
-        config = this.config
+        element.addEventListener('load', function(){
+            maxWidth = maxHeight = 0;
     
-        element.onload = function(){
-            ctx = canvas.getContext("2d");
+            for(var key in config){
+                if(options[key] != undefined && options[key] != false || options[key] == undefined){
+                    part = config[key];
+                    override = overrides[style][key];
+    
+                    if (override != undefined){
+                        partWidth = override.destX*scale + part.width*scale;
+                        maxWidth = maxWidth < partWidth ? partWidth : maxWidth;
+    
+                        partHeight = override.destY*scale + part.height*scale;
+                        maxHeight = maxHeight < partHeight ? partHeight : maxHeight;
+                    }
+                }
+            }
+    
+            canvas.height = maxHeight;
+            canvas.width = maxWidth;
+    
+            var ctx = canvas.getContext("2d");
     
             ctx.mozImageSmoothingEnabled = false;                           
             ctx.webkitImageSmoothingEnabled = false;                        
@@ -79,73 +127,30 @@ mcCanvas = {
             for(var key in config){
                 if(options[key] != undefined && options[key] != false || options[key] == undefined){
                     part = config[key];
-                    ctx.drawImage(this,
-                                  part.posX, part.posY,
-                                  part.width, part.height,
-                                  part.destX*scale, part.destY*scale,
-                                  part.width*scale, part.height*scale
-                                 );
+                    override = overrides[style][key];
+    
+                    if (override != undefined) {
+                        ctx.drawImage(this,
+                                      part.posX, part.posY,
+                                      part.width, part.height,
+                                      override.destX*scale, override.destY*scale,
+                                      part.width*scale, part.height*scale
+                                     );
+                    }
                 }
             }
     
             parent = element.parentNode;
             parent.replaceChild(canvas, element);
-        };
+        });
     
-    },
-    
-    renderFace: function(element, options){
-        scale = options.scale;
-        var canvas = document.createElement('canvas');
-        canvas.classList = element.classList;
-        canvas.id = element.id;
-    
-        canvas.width = this.config.head.width*scale;
-        canvas.height = this.config.head.height*scale;
-
-        config = this.config;
-    
-        element.onload = function(){
-            ctx = canvas.getContext("2d");
-    
-            ctx.mozImageSmoothingEnabled = false;                           
-            ctx.webkitImageSmoothingEnabled = false;                        
-            ctx.msImageSmoothingEnabled = false;                            
-            ctx.imageSmoothingEnabled = false;     
-    
-            faceConfig = {};
-            faceConfig.head = config.head;
-            faceConfig.hat = config.hat;
-    
-            for(var key in faceConfig){
-                if(options[key] != undefined && options[key] != false || options[key] == undefined){
-                    part = faceConfig[key];
-                    ctx.drawImage(this,
-                                  part.posX, part.posY,
-                                  part.width, part.height,
-                                  0*scale, 0*scale,
-                                  part.width*scale, part.height*scale
-                                 );
-                }
-            }
-    
-            parent = element.parentNode;
-            parent.replaceChild(canvas, element);
-        };
     }
 }
 
 if (window.jQuery || window.Zepto) {
-    $.fn.renderSkin = function(options){
+    $.fn.renderMinecraft = function(style, options){
         this.each(function(){
-            mcCanvas.renderSkin(this, options)
+            mcCanvas.renderMinecraft(this, options)
         });
     }
-
-    $.fn.renderFace = function(options){
-        this.each(function(){
-            mcCanvas.renderFace(this, options)
-        });
-    }
-
 }
